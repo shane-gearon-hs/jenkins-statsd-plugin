108a483e282dd88bc84c4fec06a39ddceb3911bb
Julien Eid <jeid@mesosphere.com>
2018-04-20 18:09:20 -0400

    Started converting from graphite to statsd.

--- src/main/java/jenkins/metrics/impl/graphite/PluginImpl.java
+++ src/main/java/jenkins/metrics/impl/graphite/PluginImpl.java
@@ -2,8 +2,8 @@ package jenkins.metrics.impl.graphite;
 
 import com.codahale.metrics.MetricFilter;
 import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.graphite.Graphite;
-import com.codahale.metrics.graphite.GraphiteReporter;
+
+import com.readytalk.metrics.StatsD;
 import jenkins.metrics.api.Metrics;
 import hudson.Plugin;
 import jenkins.model.Jenkins;
@@ -13,6 +13,7 @@ import org.apache.commons.lang.StringUtils;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.sql.Time;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -21,13 +22,15 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.readytalk.metrics.StatsDReporter;
 /**
  * @author Stephen Connolly
  */
+
 public class PluginImpl extends Plugin {
     private static final Logger LOGGER = Logger.getLogger(PluginImpl.class.getName());
 
-    private transient Map<GraphiteServer, GraphiteReporter> reporters;
+    private transient Map<GraphiteServer, StatsDReporter> reporters;
 
     static final String SAMPLING_INTERVAL_PROPERTY = "graphite.metrics.intervalSeconds";
 
@@ -39,7 +42,7 @@ public class PluginImpl extends Plugin {
     }
 
     public PluginImpl() {
-        this.reporters = new LinkedHashMap<GraphiteServer, GraphiteReporter>();
+        this.reporters = new LinkedHashMap<GraphiteServer, StatsDReporter>();
     }
 
     @Override
@@ -49,7 +52,7 @@ public class PluginImpl extends Plugin {
     @Override
     public synchronized void stop() throws Exception {
         if (reporters != null) {
-            for (GraphiteReporter r : reporters.values()) {
+            for (StatsDReporter r : reporters.values()) {
                 r.stop();
             }
             reporters.clear();
@@ -63,7 +66,7 @@ public class PluginImpl extends Plugin {
 
     public synchronized void updateReporters() throws URISyntaxException {
         if (reporters == null) {
-            reporters = new LinkedHashMap<GraphiteServer, GraphiteReporter>();
+            reporters = new LinkedHashMap<GraphiteServer, StatsDReporter>();
         }
         MetricRegistry registry = Metrics.metricRegistry();
         GraphiteServer.DescriptorImpl descriptor =
@@ -82,22 +85,38 @@ public class PluginImpl extends Plugin {
         for (GraphiteServer s : descriptor.getServers()) {
             toStop.remove(s);
             if (reporters.containsKey(s)) continue;
-            Graphite g = new Graphite(new InetSocketAddress(s.getHostname(), s.getPort()));
+            //Graphite g = new Graphite(new InetSocketAddress(s.getHostname(), s.getPort()));
             String prefix = StringUtils.isBlank(s.getPrefix()) ? hostname : s.getPrefix();
-            GraphiteReporter r = GraphiteReporter.forRegistry(registry)
-                    .prefixedWith(prefix)
+            //GraphiteReporter r = GraphiteReporter.forRegistry(registry)
+            //        .prefixedWith(prefix)
+            //        .convertRatesTo(TimeUnit.MINUTES)
+            //        .convertDurationsTo(TimeUnit.SECONDS)
+            //        .filter(MetricFilter.ALL)
+            //        .build(g);
+
+           // LOGGER.log(Level.INFO, "Starting Graphite reporter to {0}:{1} with prefix {2}", new Object[]{
+            //        s.getHostname(), s.getPort(), prefix
+            //});
+            //r.start(getSamplingIntervalSeconds(), TimeUnit.SECONDS);
+
+            String statsd_udp_host = System.getenv("STATSD_UDP_HOST");
+            String statsd_udp_port_str = System.getenv("STATSD_UDP_PORT");
+            int statsd_udp_port = Integer.parseInt(statsd_udp_port_str);
+
+            StatsDReporter r = StatsDReporter.forRegistry(registry)
+                    .prefixedWith("metric.prefix")
                     .convertRatesTo(TimeUnit.MINUTES)
                     .convertDurationsTo(TimeUnit.SECONDS)
                     .filter(MetricFilter.ALL)
-                    .build(g);
+                    .build(statsd_udp_host, statsd_udp_port);
+
             reporters.put(s, r);
-            LOGGER.log(Level.INFO, "Starting Graphite reporter to {0}:{1} with prefix {2}", new Object[]{
-                    s.getHostname(), s.getPort(), prefix
-            });
-            r.start(getSamplingIntervalSeconds(), TimeUnit.SECONDS);
+            r.start(10, TimeUnit.SECONDS);
+
+            LOGGER.log(Level.INFO, "Started statsd reporter");
         }
         for (GraphiteServer s: toStop) {
-            GraphiteReporter r = reporters.get(s);
+            StatsDReporter r = reporters.get(s);
             reporters.remove(s);
             r.stop();
             LOGGER.log(Level.INFO, "Stopped Graphite reporter to {0}:{1} with prefix {2}", new Object[]{
